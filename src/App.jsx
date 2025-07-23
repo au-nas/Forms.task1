d9038c3f1081fba7cb4eb2c11970df3649443b8e
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-07-23 22:05:26 +0300

    начало работы

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
 
+export const RegisterForm = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [repeatPassword, setRepeatPassword] = useState('');
 
-export const App = () => {
-	return
+	return (
+		<form>
+			<input
+				name="email"
+				type="email"
+				placeholder="Почта"
+				value={email}
+				onChange={({ target }) => setEmail(target.value)}
+			></input>
+			<input
+				name="password"
+				type="password"
+				placeholder="Пароль"
+				value={password}
+				onChange={({ target }) => setPassword(target.value)}
+			></input>
+			<input
+				name="repeatPassword"
+				type="password"
+				placeholder="Повторите пароль"
+				value={repeatPassword}
+				onChange={({ target }) => setRepeatPassword(target.value)}
+			></input>
+			<button type="submit">Зарегистрироваться</button>
+		</form>
+	);
 };
